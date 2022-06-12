ad021a5725b3ee273dacce943556c0ec014acd20
ammolder <98980721+ammolder@users.noreply.github.com>
2022-06-12 18:13:27 +0000

    Deploying to gh-pages from @ ammolder/GoUP-project@0822aa60a92ca777b4b9f5e4b279dcfe6d12dc79 🚀

---
+++ index.a102eca6.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0,dynamicBullets:!0}});
+//# sourceMappingURL=index.a102eca6.js.map
