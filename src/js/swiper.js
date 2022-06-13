dd6c9cf84181886805b68de0376340c502f7c2f1
AlinaStratij <astratijcuk@gmail.com>
2022-06-13 06:52:57 +0300

    .

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,12 +1,11 @@
 new Swiper('.swiper', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    }
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
 });
-
