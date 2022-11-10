a468aebf5d9ac559f6031f5b41b371726693fdad
ammolder <98980721+ammolder@users.noreply.github.com>
2022-11-10 18:34:01 +0000

    Deploying to gh-pages from @ ammolder/GoUP-project@a278cd578d4743768e2556d6f080a20621cd8a5c 🚀

---
+++ index.f19ea2d5.js
@@ -0,0 +1,2 @@
+!function(){var o={openLocatBtn:document.querySelector(".contact-button__btn--first"),closeLocatBtn:document.querySelector(".modal-loc__сlose-btn"),locat:document.querySelector(".backdrop-loc"),body:document.querySelector("body")};function t(){o.locat.classList.toggle("is-hidd-loc"),o.body.classList.toggle("no-scroll-loc")}o.openLocatBtn.addEventListener("click",t),o.closeLocatBtn.addEventListener("click",t)}();
+//# sourceMappingURL=index.f19ea2d5.js.map
